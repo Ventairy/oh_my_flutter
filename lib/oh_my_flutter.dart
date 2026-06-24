a4030429917ced10c4a88d32a41a0605cdab6d00
Ryan Holanda <ryanholanda08@gmail.com>
2026-06-24 18:36:09 -0300

    feat(omf): add extension to transform hex string into color

--- lib/oh_my_flutter.dart
+++ lib/oh_my_flutter.dart
@@ -5,4 +5,5 @@ library;
 export 'src/dio_interceptors/omf_offline_error_dio_interceptor.dart' show OmfOfflineErrorDioInterceptor;
 export 'src/exceptions/omf_offline_connection_dio_exception.dart' show OmfOfflineConnectionDioException;
 export 'src/extensions/color_extension.dart' show ColorExtension;
-export 'src/extensions/omf_object_extension.dart' show ObjectExtension;
+export 'src/extensions/object_extension.dart' show ObjectExtension;
+export 'src/extensions/string_extension.dart' show StringExtension;
